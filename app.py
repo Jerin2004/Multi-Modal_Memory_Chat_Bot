eafbd4b444863b87ac167d9b426324a3d4428235
jerin <jerinjabraham45@gmail.com>
2025-07-01 09:47:08 +0530

    Updated app.py with new changes

--- app.py
+++ app.py
@@ -61,6 +61,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 import os
+from langchain_community.embeddings import HuggingFaceEmbeddings
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 # ---------------------- Styling ---------------------- #
 st.markdown("""
